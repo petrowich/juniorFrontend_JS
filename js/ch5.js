f520036a7b960bfbc1a84bdfdc7383003adb397f
Petr Kreslin <petr.kreslin@gmail.com>
2017-10-06 03:29:21 +0300

    commit 3

--- js/ch5.js
+++ js/ch5.js
@@ -38,12 +38,19 @@ function sum(a) {
   }
 
 
+
+
 function ch5_ReverseArray(a) {
-    console.log(ReverseArray(["A", "B", "C"]));
-    console.log(reverseArrayInPlace(["A", "B", "C", "D", "E", "F", "G", "H"]));
+    console.clear();
+    console.log(reverseArray(["A", "B", "C"]));
+    // → ["C", "B", "A"];
+    var arrayValue = [0, 1, 2, 3, 4, 5];
+    reverseArrayInPlace(arrayValue);
+    console.log(arrayValue);
+    // → [5, 4, 3, 2, 1]
 }
 
-function ReverseArray(a) {
+function reverseArray(a) {
     var b = [];
         
     for (var i = a.length - 1; i >= 0; i--)
@@ -51,30 +58,74 @@ function ReverseArray(a) {
     return b;
 };
 
-function reverseArrayInPlace(arr, i) {
-    if(!!!i)    //если аргумент i(индекс массива) не указан
+function reverseArrayInPlace(a, i) {
+    if(!!!i)    //если аргумент i (индекс элемента) не указан, т.к. официально второго аргумента по условиям задачи у этой функции нет, то он 0й
         i = 0;
 
-    var n = (arr.length-1) - i;
+    var n = (a.length-1) - i; //зеркальный индекс от хвоста массива
 
-    console.log(i, n);
-
-    if ((n-i)>1) {
-        var temp = arr[i];
-        arr[i] = arr[n];
-        arr[n] = temp;
-        return reverseArrayInPlace(arr, i+1);
+    if (i < n) {              //если передний индекс впереди заднего - меняем значения элементов местами
+        var temp = a[i];
+        a[i] = a[n];
+        a[n] = temp;
+        return reverseArrayInPlace(a, i+1); // и рекурсивно вызываем функцию, указав вторым аргументом следующий индекс элемента
     }
     else
-        return arr;    
+        return a;
 }
 
 
 
 
 function ch5_List() {
-    console.log("не готово");
+    console.clear();
+    console.log(arrayToList([10, 20]));
+    // → {value: 10, rest: {value: 20, rest: null}}
+    console.log(listToArray(arrayToList([10, 20, 30])));
+    // → [10, 20, 30]
+    console.log(prepend(10, prepend(20, null)));
+    // → {value: 10, rest: {value: 20, rest: null}}
+    console.log(nth(arrayToList([10, 20, 30]), 1));
+    // → 20
 };    
+
+function arrayToList(arr) {
+	for (var i = arr.length - 1; i >= 0; i--) {
+		var nextRest = list || null;
+		var list = {};
+
+		list["value"] = arr[i];
+		list["rest"] = nextRest;
+	}
+
+	return list;
+}
+
+
+function listToArray(list) {
+	var arr = [];
+	arr.push(list.value);
+	var rest = list.rest;
+
+	while (rest) {
+		arr.push(rest.value);
+		rest = rest["rest"];
+	}
+
+	return arr;
+}
+
+
+function prepend(elem, list) {
+	return {value: elem, rest: list}
+}
+
+
+function nth(list, index) {
+	if (!list.rest && index != 0) return undefined;
+	return (index != 0) ? nth(list.rest, --index) : list.value;
+}
+
 
 function ch5_Deep_Comparison() {
     console.log("не готово");    
